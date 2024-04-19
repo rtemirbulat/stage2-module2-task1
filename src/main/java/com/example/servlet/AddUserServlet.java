874b1724750a0f83dfcaf948d6ba64f7582acb13
rtemirbulat <equalityisnotequality@gmail.com>
2024-04-19 13:48:37 +0500

    finished

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -1,5 +1,26 @@
 package com.example.servlet;
 
-public class AddUserServlet {
-    //write your code here!
+import com.example.User;
+import com.example.Warehouse;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@WebServlet("/add")
+public class AddUserServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws  ServletException, IOException{
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+
+        User user = new User(firstName,lastName);
+        Warehouse.getInstance().addUser(user);
+
+        request.setAttribute("user",user);
+
+        request.getRequestDispatcher("/add.jsp").forward(request,response);
+
+    }
 }
